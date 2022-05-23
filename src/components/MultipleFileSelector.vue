c37ddd64d07769db502f7bbf4624c435bb44355e
Rehman Ahmed Khan <72739116+rehmankha73@users.noreply.github.com>
2022-05-23 16:44:31 +0500

    adding preview for video(when user uploads video, it shows preview of video).

--- src/components/MultipleFileSelector.vue
+++ src/components/MultipleFileSelector.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="drop-box display-inline align-center mt-6" @dragover.prevent @drop=" onDrop "
+  <div class="drop-box display-inline align-center mt-6" @dragover.prevent @drop="onDrop"
        @click="files.length === 0 ? $refs['file-input'].click() : ''"
   >
     <label v-if="files.length === 0" class="input-message mt-2">
       {{ label }}
       <input class="file-input" ref="file-input" type="file" name="image" @change="onChange" multiple>
     </label>
-    <label v-else class="hidden display-inline align-center mx-4" :class="{ 'image': true }">
+    <label v-else class="display-inline align-center mx-4">
       <div v-for="(file, index) in files" class="d-flex justify-content-between my-2">
 
-        <img v-if="file && fileType === 'image/*'" :src="file" :alt="file.name" width="50px" height="50px"
+        <img v-if="file && fileType === 'image/*'" :src="file" :alt="file.name"
              class="image-preview" />
 
-        <img v-if="file && fileType === 'application/*'" src="../assets/file_icon.jpg" :alt="file.name"
-             style="width: 50px; height: 50px" />
+        <embed v-if="file && fileType === 'application/*'" :src="file"
+               class="d-block" style="width: 100px; height: 100px" />
 
-        <img v-if="file && fileType === 'video/*'" src="../assets/video_icon.png" :alt="file.name"
-             style="width: 50px; height: 50px" />
+        <img v-if="file && fileType === 'video/*'" :src="video_thumbnails[index]" :alt="file.name"
+             class="d-block" style="width: 100px; height: 100px" />
 
-        <p class="mt-4">
+        <p class="mt-4 d-block">
           Name: {{ files_data[index].name }}<br>
           Extensions: {{ files_data[index].type }}<br>
         </p>
 
-        <span v-if="file"
-              class="mt-2 text-danger border-0 cursor-pointer" @click.stop="removeFile(index)">X</span>
+        <span v-if="file" class="mt-2 text-danger border-0 cursor-pointer d-block"
+              @click.stop="removeFile(index)">X</span>
       </div>
     </label>
   </div>
@@ -59,7 +59,6 @@
 
   </div>
 
-
 </template>
 
 <script>
@@ -67,11 +66,12 @@ export default {
   name: "FileSelector",
   data() {
     return {
+      video_thumbnails: [],
       error: "",
       no_of_files: "",
       files_data: [],
       files: [],
-      fileType: "video/*"
+      fileType: "image/*"
     };
   },
   props: {
@@ -91,12 +91,52 @@ export default {
   watch: {
     files: {
       handler(newVal) {
+        if(newVal.length === 0) {
+          this.video_thumbnails = [];
+        }
         this.no_of_files = this.files.length;
         this.$emit("getUploadedFiles", this.files);
       }, deep: true
     }
   },
   methods: {
+    async generateThumbnail(item) {
+      console.log(item);
+      console.log("thumbnail");
+      const binaryData = [];
+      binaryData.push(item);
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      const video = document.createElement("video");
+      video.setAttribute("src", URL.createObjectURL(new Blob(binaryData)));
+      video.load();
+      let thumbnail = await new Promise((resolve) => {
+        video.onloadedmetadata = async () => {
+          console.log("in onload");
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+          video.currentTime = video.duration / 2;
+          await video.play();
+          context.drawImage(video, 0, 0);
+          video.pause();
+          const blob = await new Promise((resolve) => {
+            return canvas.toBlob(function(blob) {
+              resolve(URL.createObjectURL(blob));
+            });
+
+            // return canvas.toDataURL();
+          });
+          console.log(blob);
+          resolve(blob);
+        };
+      });
+
+      console.log(thumbnail, "thumb");
+
+      this.video_thumbnails.push(thumbnail);
+      // return thumbnail;
+    },
+
     onDrop: function(e) {
       e.stopPropagation();
       e.preventDefault();
@@ -133,17 +173,20 @@ export default {
       this.createFile(_valid_files);
     },
 
-    createFile(files) {
-
-      for (let i = 0; i < files.length; i++) {
+    createFile(_files) {
+      for (let i = 0; i < _files.length; i++) {
         let reader = new FileReader();
         let vm = this;
 
-        reader.readAsDataURL(files[i]);
+        reader.readAsDataURL(_files[i]);
 
         reader.onload = function(e) {
           vm.files[i] = reader.result;
         };
+
+        if (_files[i].type.match("video/*")) {
+          this.generateThumbnail(_files[i]);
+        }
 
       }
 
@@ -151,6 +194,7 @@ export default {
 
     removeFile(_index) {
       this.files.splice(_index, 1);
+      this.video_thumbnails.splice(_index, 1);
     }
   }
 };
